e2f16a4f5cbe40df87bf22ed7ea2f7a495762d8b
sasha (home) <facosta@student.42madrid.com>
2024-09-28 19:30:57 +0200

    feat: isprint

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 22:03:09 by facosta           #+#    #+#             */
-/*   Updated: 2024/09/28 19:24:00 by facosta          ###   ########.fr       */
+/*   Updated: 2024/09/28 19:26:54 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
 
 void	*ft_memset(void *s, int c, size_t n);
 
