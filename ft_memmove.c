19d75125e990babe71d42d5efe6624f9a99f78bb
sasha (home) <facosta@student.42madrid.com>
2024-09-28 20:54:07 +0200

    feat: memmove
    & add. changes to memcpy

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/28 19:50:50 by facosta           #+#    #+#             */
-/*   Updated: 2024/09/28 20:35:12 by facosta          ###   ########.fr       */
+/*   Created: 2024/09/28 20:26:37 by facosta           #+#    #+#             */
+/*   Updated: 2024/09/28 20:51:39 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 // #include <string.h>  // EXT
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = 0;
 	if (dest || src)
 	{
-		while (i < n)
-		{
-			ft_memset(dest + i, (int) *(unsigned char *)(src + i), 1);
-			i++;
-		}
+		if (dest < src)
+			return (ft_memcpy(dest, src, n));
+		while (n--)
+			ft_memset(dest + n, (int) *(unsigned char *)(src + n), 1);
 	}
 	return (dest);
 }
@@ -33,9 +29,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 // int	main(void)
 // {
 // 	char	src[] = "sfidfjhgfdg jhf\n ef";
-// 	char	dest[100] = "me van a sobreescribir :(";
+// 	char	dest[1];
 
 // 	printf("before: %s\n", dest);
-// 	ft_memcpy(dest, src, 10);
+// 	memmove(dest, src, 10);
 // 	printf("after:  %s\n", dest);
 // }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 22:03:09 by facosta           #+#    #+#             */
-/*   Updated: 2024/09/28 19:50:28 by facosta          ###   ########.fr       */
+/*   Updated: 2024/09/28 20:42:20 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int		ft_isprint(int c);
 
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
