9596002535671e003d92b79f801bed6a5fe65078
sasha (home) <facosta@student.42madrid.com>
2024-09-28 19:11:30 +0200

    feat: isdigit

--- libft.h
+++ libft.h
@@ -6,13 +6,16 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 22:03:09 by facosta           #+#    #+#             */
-/*   Updated: 2024/09/25 22:18:36 by facosta          ###   ########.fr       */
+/*   Updated: 2024/09/28 19:07:16 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 
 void	*ft_memset(void *s, int c, size_t n);
 
