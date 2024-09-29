7865af40aade3cb7b8450bd23bffc0ad4eb16d27
sasha (home) <facosta@student.42madrid.com>
2024-09-29 17:51:26 +0200

    fix: putendl_fd in proper file!!

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,8 +6,20 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 16:00:41 by facosta           #+#    #+#             */
-/*   Updated: 2024/09/29 16:01:40 by facosta          ###   ########.fr       */
+/*   Updated: 2024/09/29 17:51:00 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "libft.h"
+
+void ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	write(fd, "\n", 1);
+}
+
+// int main(void)
+// {
+// 	ft_putendl_fd("hello world :)", 1);
+// }
