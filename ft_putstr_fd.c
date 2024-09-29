89f1ca54507936e3cf6c825836ab7aa8678d4279
sasha (home) <facosta@student.42madrid.com>
2024-09-29 17:42:39 +0200

    feat: putstr_fd

---
+++ ft_putstr_fd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 16:00:31 by facosta           #+#    #+#             */
+/*   Updated: 2024/09/29 17:41:05 by facosta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (s)
+		write(fd, s, ft_strlen(s));
+}
+
+// int	main(void)
+// {
+// 	char	str[20] = "Hello World :)";
+
+// 	ft_putstr_fd(str, 1);
+// }
