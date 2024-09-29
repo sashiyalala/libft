b649cddda0157c7c31535aca9cf0241eebfaef2a
sasha (home) <facosta@student.42madrid.com>
2024-09-29 18:09:41 +0200

    feat: putendl_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,14 +6,14 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 16:00:41 by facosta           #+#    #+#             */
-/*   Updated: 2024/09/29 17:51:00 by facosta          ###   ########.fr       */
+/*   Updated: 2024/09/29 18:09:22 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
