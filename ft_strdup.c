06d8d557bc90ec65adc226ba370c296e1f7ce387
sasha (home) <facosta@student.42madrid.com>
2024-09-29 12:48:25 +0200

    feat: strdup

---
+++ ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 01:20:28 by facosta           #+#    #+#             */
+/*   Updated: 2024/09/29 01:27:54 by facosta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>  // EXT
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	size_t	len;
+
+	len = ft_strlen(s) + 1;
+	dest = malloc(len);
+	if (!dest)
+		return (dest);
+	ft_memcpy(dest, s, len);
+	return (dest);
+}
+
+// int	main(void)
+// {
+// 	char	str[20] = "hello world :)";
+// 	char	*dest = ft_strdup(str);
+// 	printf("%s\n", dest);
+// 	free(dest);
+// }
