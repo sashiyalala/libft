ce1874858e7c1a1f6c958c2cee3dce9d9027afbb
sasha (home) <facosta@student.42madrid.com>
2024-09-29 18:31:50 +0200

    feat: striteri

---
+++ ft_striteri.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 16:00:09 by facosta           #+#    #+#             */
+/*   Updated: 2024/09/29 18:31:42 by facosta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	k;
+
+	k = 0;
+	if (s)
+	{
+		while (*(s + k))
+		{
+			(*f)(k, s + k);
+			++k;
+		}
+	}
+}
+
+// void	ft_test_fn(unsigned int i, char *s)
+// {
+// 	if (i % 2)
+// 		(*s)++;
+// 	else
+// 		(*s)--;
+// }
+
+// int	main(void)
+// {
+// 	char	str[34] = "hello world :)";
+// 	ft_striteri(str, ft_test_fn);
+// 	ft_putendl_fd(str, 1);
+// }
