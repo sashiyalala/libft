a873eedacd65ccf4b9fde4b4b764edd899cc6e30
sasha (home) <facosta@student.42madrid.com>
2024-09-29 18:09:51 +0200

    feat: putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 16:00:52 by facosta           #+#    #+#             */
+/*   Updated: 2024/09/29 18:09:08 by facosta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	help_char;
+
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	if (n / 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		help_char = '0' + n;
+		write(fd, &help_char, 1);
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_putnbr_fd(-2147483648, 1);
+// 	ft_putchar_fd('\n', 1);
+// 	ft_putnbr_fd(-0, 1);
+// 	ft_putchar_fd('\n', 1);
+// 	ft_putnbr_fd(34832, 1);
+// }
