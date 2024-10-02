ae6a4ab7cd9139afafb27c99d5ea0dafeae57e8e
sasha (home) <facosta@student.42madrid.com>
2024-10-02 20:17:30 +0200

    style: changes aspect of block comment

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 19:29:15 by facosta           #+#    #+#             */
-/*   Updated: 2024/10/02 00:40:39 by facosta          ###   ########.fr       */
+/*   Updated: 2024/10/02 20:16:02 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 // #include <stdlib.h>  // EXT
 #include "libft.h"
 
-/*
-Create a new node using `malloc`. The attribute `content` is initialized with
-the content of the argument `content`. The attribute `next` is initialized with
-*/
+// Create a new node using `malloc`. The attribute `content` is initialized with
+// the content of the argument `content`. The attribute `next` is initialized
+// with
 
 t_list	*ft_lstnew(void *content)
 {
